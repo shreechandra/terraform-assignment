c44cb4ab9b907a48a7a4c3e9205102598c9616db
shreechandra <100112433+shreechandra@users.noreply.github.com>
2022-08-10 17:53:40 +0530

    Add files via upload

---
+++ cloudFront.tf
@@ -0,0 +1,74 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket = "assign-bucket"
+  tags = {
+    Name        = "Assign_bucket123"
+  }
+  versioning {
+    enabled = true
+  }
+}
+resource "aws_s3_bucket_acl" "b_acl" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "log-delivery-write"
+}
+locals {
+  s3_origin_id = "myS3Origin"
+}
+resource "aws_cloudfront_origin_access_identity" "example" {
+  comment = "Some comment"
+}
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.bucket.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.example.cloudfront_access_identity_path
+    }
+  }
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Some comment"
+  default_root_object = "index.html"
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+  ordered_cache_behavior {
+    path_pattern     = "/content/immutable/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = local.s3_origin_id
+    forwarded_values {
+      query_string = false
+      headers      = ["Origin"]
+      cookies {
+        forward = "none"
+      }
+    }
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+  price_class = "PriceClass_200"
+  viewer_certificate {
+    cloudfront_default_certificate = true
+    }
+    restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+}
