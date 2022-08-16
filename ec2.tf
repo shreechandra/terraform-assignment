4ccfa37370dc065d01d9f19da0f2461978d649bf
shreechandra <100112433+shreechandra@users.noreply.github.com>
2022-08-16 15:58:34 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,23 +1,16 @@
-  associate_public_ip_address = true
-  subnet_id = aws_subnet.mysubnet1.id
-  vpc_security_group_ids = [aws_security_group.allow.id]
-  iam_instance_profile =  "${aws_iam_instance_profile.ec2_shree}"
-
-  tags = {
-    Name = "instant"
-  }
-  volume_tags = {
-    "Name" = "terraform"
-  }
-  user_data = <<-EOF
-                #!/bin/bash
-                sudo apt update -y
-                
-                sudo apt install ruby-full -y
-                sudo apt install wget -y 
-                sudo wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
-                sudo chmod +x ./install
-                sudo ./install auto  
-                sudo service codedeploy-agent status 
-                sudo service codedeploy-agent restart
-              EOF
+  resource "aws_instance" "myInstance" {
+  ami           = "ami-0b02eacf129bfac4e"
+  instance_type = "t2.micro"
+  key_name = "redgear"
+  tags                         = {
+        "Name" = "instance1"
+    }
+    user_data = <<-EOF
+                  #!/bin/bash
+                  sudo apt update -y
+                  sudo apt-get install -y apache2
+                  sudo systemctl start apache2
+                  sudo systemctl enable apache2
+                  sudo apt update
+                EOF
+}
