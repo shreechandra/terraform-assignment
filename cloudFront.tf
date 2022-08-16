7a24697ff5013ba0a3ba9d254eb03339586fec7b
shreechandra <100112433+shreechandra@users.noreply.github.com>
2022-08-16 15:17:23 +0530

    Update cloudFront.tf

--- cloudFront.tf
+++ cloudFront.tf
@@ -1,5 +1,5 @@
-resource "aws_s3_bucket" "bucket" {
-  bucket = "assign-bucket"
+resource "aws_s3_bucket" "buckettt95" {
+  bucket = "assign-buckettt95"
   tags = {
     Name        = "Assign_bucket123"
   }
